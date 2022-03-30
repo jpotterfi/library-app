4a0e4adeb894b6a51dac958b84e6ab6fef12f09b
Jeremy Potterfield <Jeremy.Potterfield@gmail.com>
2022-03-30 00:37:56 -0400

    added delete buttons to cards and changeReadStatus protoype to Book objects

--- myscripts.js
+++ myscripts.js
@@ -3,6 +3,18 @@ let submit = document.getElementById("submit");
 let rightContent = document.getElementById("right-content");
 let order = 0;
 
+function Status() {
+}
+
+Status.prototype.changeReadStatus = function() {
+    if  (this.read == "true") {
+         this.read = "false";
+    } else if 
+        (this.read == "false") { 
+         this.read = "true";
+    }
+}
+
 function Book(author, title, numberOfPages, read, genre, order){
     this.author = author;
     this.title = title;
@@ -11,6 +23,8 @@ function Book(author, title, numberOfPages, read, genre, order){
     this.genre = genre;
     this.order = order;
 }
+
+Book.prototype = Object.create(Status.prototype);
 
 submit.addEventListener("click", function(){
   let author = document.getElementById("author").value;
@@ -28,83 +42,117 @@ submit.addEventListener("click", function(){
    const newBook = new Book(author, title, numberOfPages, readStatus, genre, order);
    myLibrary.push(newBook);
 
-   console.table(newBook)
-  
-//myLibrary.push(new Book(author, title, numberOfPages, readStatus, genre, order));
 
+   let cleanSlate = document.getElementById("right-content");
+   cleanSlate.innerHTML = '';
 
-console.table(myLibrary);
-  
-
-////bookCard styling//////
-
-    const bookCard = document.createElement("div");
-    bookCard.className = "bookCard"
-    bookCard.style.width = "250px";
-    bookCard.style.height = "300px";
-    bookCard.style.borderRadius = "8px";
-    //bookCard.style.border = "2px black solid"
-    bookCard.style.borderLeft = "12px #F0B428 solid";
-    //bookCard.style.borderTopLeftRadius = "4px";
-    //bookCard.style.borderBottomLeftRadius = "4px";
-    bookCard.style.minWidth = "250px";
-    bookCard.style.minHeight = "300px";
-    bookCard.style.backgroundColor = "white";
-    bookCard.style.margin = "10px";
-    bookCard.style.boxShadow = "0 2px 5px rgba(0, 0, 0, 0.25)";
-    bookCard.style.display = "flex";
-    bookCard.style.flexDirection = "column";
-
+  for (i=0; i < myLibrary.length; i++) {
     
-    
-////bookCard styling//////
-
-///bookCard content formatting///
 
 
+        ////bookCard styling//////
 
-    //let currentBook = myLibrary[order];
-    //console.log("currentBook is " + currentBook);
+            const bookCard = document.createElement("div");
+            bookCard.className = "bookCard"
+            bookCard.id = "bookCard" + i;
+            bookCard.style.width = "250px";
+            bookCard.style.height = "300px";
+            bookCard.style.borderRadius = "8px";
+            //bookCard.style.border = "2px black solid"
+            bookCard.style.borderLeft = "12px #F0B428 solid";
+            //bookCard.style.borderTopLeftRadius = "4px";
+            //bookCard.style.borderBottomLeftRadius = "4px";
+            bookCard.style.minWidth = "250px";
+            bookCard.style.minHeight = "300px";
+            bookCard.style.backgroundColor = "white";
+            bookCard.style.margin = "10px";
+            bookCard.style.boxShadow = "0 2px 5px rgba(0, 0, 0, 0.25)";
+            bookCard.style.display = "flex";
+            bookCard.style.flexDirection = "column";
 
-    let bookTitle = document.createElement("div");
-    bookTitle.className = "bookTitle";
-    bookTitle = myLibrary[0].title;
+            
+            
+        ////bookCard styling//////
 
-    let bookAuthor = document.createElement("div");
-    bookAuthor.className = "bookAuthor";
-    bookAuthor = newBook.author;
-
-    let bookNumberOfPages = document.createElement("div");
-    bookNumberOfPages.className = "bookTitle";
-    bookNumberOfPages = newBook.numberOfPages;
-
-    let bookGenre = document.createElement("div");
-    bookGenre.className = "bookTitle";
-    bookGenre = newBook.genre;
-
-    let bookReadStatus = document.createElement("div");
-    bookReadStatus.className = "bookReadStatus";
-    bookReadStatus = newBook.read;
-
-    let bookOrder = document.createElement("div");
-    bookOrder.className = "bookOrder";
-    bookOrder = newBook.order;
-
-
-    console.log(bookTitle + bookAuthor + bookNumberOfPages
-        + bookGenre + bookReadStatus + bookOrder);
-    //const bookTitle = document.createElement("div");
-   // bookTitle.className = "title";
-    //bookTitle = myLibrary[order.title];
-    //console.log("Book Title is" + bookTitle);
+        ///bookCard content formatting///
 
 
 
+            //let currentBook = myLibrary[order];
+            //console.log("currentBook is " + currentBook);
 
-///bookCard content formatting///
 
-    rightContent.appendChild(bookCard);
-    order++;
+
+            let bookTitle = document.createElement("div");
+            bookTitle.className = "bookTitle";
+            bookTitle = myLibrary[i].title;
+
+            let bookAuthor = document.createElement("div");
+            bookAuthor.className = "bookAuthor";
+            bookAuthor = myLibrary[i].author;
+
+            let bookNumberOfPages = document.createElement("div");
+            bookNumberOfPages.className = "bookTitle";
+            bookNumberOfPages = myLibrary[i].numberOfPages;
+
+            let bookGenre = document.createElement("div");
+            bookGenre.className = "bookTitle";
+            bookGenre = myLibrary[i].genre;
+
+            let bookReadStatus = document.createElement("div");
+            bookReadStatus.className = "bookReadStatus";
+            bookReadStatus = myLibrary[i].read;
+
+            let bookOrder = document.createElement("div");
+            bookOrder.className = "bookOrder";
+            bookOrder = myLibrary[i].order;
+
+
+
+            console.log(bookTitle + bookAuthor + bookNumberOfPages
+                + bookGenre + bookReadStatus + bookOrder);
+
+
+            let deleteButton = document.createElement("button");
+            deleteButton.className = "delete";
+            deleteButton.id = i;
+            deleteButton.style.width = "100px";
+            deleteButton.style.height = "100px";
+            deleteButton.style.minWidth = "100px";
+            deleteButton.style.minHeight = "100px";
+
+            bookCard.appendChild(deleteButton);
+
+            deleteButton.addEventListener("click", function(){
+                const element = document.getElementById("bookCard" + deleteButton.id);
+                myLibrary.splice(deleteButton.id, 1);
+                order--;
+                element.remove();
+                console.table(myLibrary);
+            });
+
+
+
+            
+
+            //const bookTitle = document.createElement("div");
+        // bookTitle.className = "title";
+            //bookTitle = myLibrary[order.title];
+            //console.log("Book Title is" + bookTitle);
+
+            /*testing protoype
+            console.table(newBook);
+            newBook.changeReadStatus();
+            console.table(newBook);
+            testing protoype*/    
+
+        ///bookCard content formatting///
+
+            rightContent.appendChild(bookCard);
+            order++;
+
+  } //end of loop
+
 });
 
 
